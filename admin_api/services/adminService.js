3defedbb32158f65a5a98c1110f07170c36d749c
carlosjfh <carlos.flores@unas.edu.pe>
2024-08-13 10:53:57 -0500

    cambiar dni por id

--- admin_api/services/adminService.js
+++ admin_api/services/adminService.js
@@ -1,13 +1,13 @@
 const axios = require("axios");
 
 module.exports = {
-    // Obtener la información de un usuario por su DNI
-    getUsuarioByDni: async function(dni_usuario) {
+    // Obtener la información de un usuario por su id
+    getUsuarioByid: async function(id_usuario) {
         try {
-            const response = await axios.get(`http://usuarios_api:81/usuario/${dni_usuario}`);
+            const response = await axios.get(`http://usuarios_api:81/usuario/${id_usuario}`);
             return response.data[0]; 
         } catch (error) {
-            console.error(`Error al obtener usuario con DNI ${dni_usuario}:`, error);
+            console.error(`Error al obtener usuario con id ${id_usuario}:`, error);
             throw error;
         }
     },
@@ -29,28 +29,28 @@ module.exports = {
     },
 
     // Método para editar un usuario
-    updateUsuario: async function(dni_usuario, usuarioData) {
+    updateUsuario: async function(id_usuario, usuarioData) {
         try {
             // Validar que los datos cumplan con el esquema de usuario antes de enviar
             if (!usuarioData.nombre || !usuarioData.email || !usuarioData.telefono || !usuarioData.contrasena) {
                 throw new Error('Faltan campos requeridos en usuarioData');
             }
 
-            const response = await axios.put(`http://usuarios_api:81/usuario/${dni_usuario}`, usuarioData);
+            const response = await axios.put(`http://usuarios_api:81/usuario/${id_usuario}`, usuarioData);
             return response.data;
         } catch (error) {
-            console.error(`Error al actualizar usuario con DNI ${dni_usuario}:`, error);
+            console.error(`Error al actualizar usuario con id ${id_usuario}:`, error);
             throw error;
         }
     },
 
     // Método para eliminar un usuario
-    deleteUsuario: async function(dni_usuario) {
+    deleteUsuario: async function(id_usuario) {
         try {
-            const response = await axios.delete(`http://usuarios_api:81/usuario/${dni_usuario}`);
+            const response = await axios.delete(`http://usuarios_api:81/usuario/${id_usuario}`);
             return response.data;
         } catch (error) {
-            console.error(`Error al eliminar usuario con DNI ${dni_usuario}:`, error);
+            console.error(`Error al eliminar usuario con id ${id_usuario}:`, error);
             throw error;
         }
     }
